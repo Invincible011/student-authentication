4e669def65608f919a6d4188a707f4812decaf37
paritieltd <paritieltd@gmail.com>
2022-10-13 09:21:13 +0100

    Modifies the database file

--- config.py
+++ config.py
@@ -1,23 +1,25 @@
 import sqlite3
 
-conn = sqlite3.connect('./database/app.db')
+class Connection:
+    def connect_database(self):
+        self.conn = sqlite3.connect('./database/app.db')
+        self.c = self.conn.cursor()
+        self.c.execute("""CREATE TABLE students(
+            first text,
+            middle text,
+            surname text,
+            course text
+            )""")
+        self.conn.commit()
+    
+    def insert_into_database(self):
+        self.c.execute("INSERT INTO students values ('AbdulMumin', 'Onoroyiza', 'Adeyemo', 'MAT112')")
+        self.conn.commit()
+    
+    def query_from_database(self, query, database, search):
+        self.c.execute("SELECT {self.query} FROM {self.database} WHERE first='{self.search}'")
+        print(self.c.fetchall())
+        self.conn.commit()
 
-c = conn.cursor()
-
-'''c.execute("""CREATE TABLE students(
-    first text,
-    middle text,
-    surname text,
-    course text
-    )""")
-'''
-
-conn.commit()
-#c.execute("INSERT INTO students values ('AbdulMumin', 'Onoroyiza', 'Adeyemo', 'MAT112')")
-
-c.execute("SELECT * FROM students WHERE first='AbdulMumin'")
-print(c.fetchall())
-
-conn.commit()
-
-conn.close()
+    def exit_database(self):
+        self.conn.close()
