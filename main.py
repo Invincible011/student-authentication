00cdffe267b27ca10250cc6a3ebe7639b52be251
Invincible011 <abdulibnthani@gmail.com>
2022-10-15 15:11:34 +0100

    Fix bugs

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from faculty import Faculty
+from config import Configure
+from sqlite3 import Error
 
 if __name__ == "__main__":
     fac = Faculty()
-    print(fac.add_course('Csc224'))
-    print(fac.add_course('Csc214'))
-    print(fac.add_course('Csc243'))
-    print(fac.add_course('gbh231'))
-    print('These are all your Course: ',fac.get_courses())
-
-    #fac.calculate_gpa()
-    #print(fac.show_gpa())
-    print(fac.get_courses())
-    print(fac.show_remark())
+    
