55e646e8f00ecaecdc9edf0de35275984caa7450
Invincible011 <abdulibnthani@gmail.com>
2022-11-08 17:08:06 +0100

    Update and Fix Bugs

--- main.py
+++
@@ -1,5 +0,0 @@
-from faculty import Faculty
-
-if __name__ == "__main__":
-    fac = Faculty()
-    fac.calculate_gpa()
--- student/student.py
+++ student/student.py
@@ -13,6 +13,7 @@ from kivy.properties import ListProperty, ObjectProperty
 Builder.load_file('personal_details.kv')
 main_win = Builder.load_file('studdash.kv')
 
+
 class Dropdown(BoxLayout):
     button_list = ListProperty(None)
     
@@ -21,14 +22,10 @@ class Dropdown(BoxLayout):
         self.button_list = button_list     
     
     def on_button_list(self, object, values):
-        '''size_hint_y= None
-        height = 40 
-        bold = True
-        color = (1, 1, 1, .8)
-        background_color = box_bg_color
-        background_normal = '''
+        
         for i in values:
             self.add_widget(Button(text=i, font_size= 9, background_color=(0,0,0,.2), background_normal='', bold=True))
+
 
 class NewBox(BoxLayout):
     dropdown = ObjectProperty(None)
@@ -56,14 +53,15 @@ class NewBox(BoxLayout):
         self.dropdown = widget
         for w in wid:
             self.add_widget(w)
+
         
 class MainDashboard(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
     
     def togglemenutab(self):
-        toggle = self.ids.menu
-        
+
+        toggle = self.ids.menu        
         if toggle.width == 140:
             toggle.width = 0
         else:
@@ -81,9 +79,11 @@ class MainDashboard(BoxLayout):
         
         log.text = f"Today: {week}, {month}, {year}."
         
+        
 class StudentApp(App):
     def build(self):
         return MainDashboard()
+        
         
 if __name__ == "__main__":
     st = StudentApp()
